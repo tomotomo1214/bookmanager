b0cbc2a29a75ad303c6fe32d6f5307adcbe6e3ee
tomotomo <kids.kabuki2018@gmail.com>
2023-09-30 14:34:47 +0900

    model add

--- book/models.py
+++ book/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=64)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    detail=models.TextField()
+
