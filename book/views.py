f60d49a12e58b782ac0885073deb34fd115bb1bf
tomotomo <kids.kabuki2018@gmail.com>
2023-12-09 15:56:41 +0900

    form

--- book/views.py
+++ book/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Book
-# Create your views here.
+from django.http import HttpResponse
+from .forms import PostForm
+
+def new_book(request):
+    parms={
+        'title':'本登録ページ',
+        'form':PostForm
+    }
+    if (request.method=='POST'):
+        title=request.POST['title']
+        print(title)
+    return render(request,'form.html',parms)
--- bookmanager/urls.py
+++ bookmanager/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from book.urls import urlpatterns as book_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('book/',include(book_urlpatterns))
 ]
